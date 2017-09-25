184d527c5a11e6a035e680bd0d07c8f6d5b56c7b
aimiy <mioly@foxmail.com>
2017-09-25 17:42:34 +0800

    new

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -6,6 +6,8 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    
+    var err = 430;
 
     // clear out old data before new request
     $wikiElem.text("");
@@ -15,8 +17,8 @@ function loadData() {
     var cityStr = $('#city').val();
     var address = streetStr + '' +cityStr;
     
-    $greeting.text('你想住在' + address +'吗？');
     
+    $greeting.text('你想住在' + address +'吗？');
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + address +'';
     
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
@@ -26,30 +28,45 @@ function loadData() {
     // YOUR CODE GOES HERE!
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
+	'q':address,
   'api-key': "821f70e5f21b43c495bb519eceab47d3"
 });
 $.ajax({
   url: url,
   method: 'GET',
 }).done(function(result) {
-
-  
-  console.log(result.response.docs[0].snippet);
+	
   var news = result.response.docs;
-  console.log(news);
-
-  for (var i =0;i<news.length;i++){
-    var newsList = '<li class="article-list">';
-    newsList += news[i].snippet;
-    newsList += '</li>';    
-    $("#nytimes-articles").append(news[i].snippet);
-  }
   
-
-
+  for (var i =0;i<news.length;i++){
+    var newsList = '<li class="article-list"><a href="' + news[i].web_url + '">'+news[i].headline.main + '</a>' + '<p>' + news[i].snippet + '</li>';
+    $nytElem.append(newsList);
+  }
 }).fail(function(err) {
+	$nytElem.append('请求出错');
   throw err;
 });
+
+//jsonp由于后台发生的情况没有错误操作，因此采用计时器
+
+var wikiRequestTimeout = setTimeout(function(){
+	$wikiElem.append("请求超时");
+},6000);
+
+$.ajax({
+	dataType:"jsonp",
+	url:"https://en.wikipedia.org/w/api.php?action=opensearch&search="+ address +"&format=json"
+}).done(function(result){
+	console.log(result);
+	for(var i = 0; i<result[1].length;i++){
+		var wikisearch = '<li><a href="' + result[3][i] +'">' + result[1][i] +'</a></li>';
+		$wikiElem.append(wikisearch);
+	}
+	clearTimeout(wikiRequestTimeout);
+});
+
+
+
 
     return false;
 };
