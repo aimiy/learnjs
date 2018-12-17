537ac61d24c9033f8dcf48c666a06e9af9fb61b7
aimiy <mioly@foxmail.com>
2018-12-17 17:32:25 +0800

    webpack4

--- webpack4/src/index.js
+++ webpack4/src/index.js
@@ -1,9 +1,26 @@
 import _ from 'lodash';
+import './style.css';
+import img from './rocket.jpg';
+import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+
+  var myjpg = new Image();
+  myjpg.src = img;
+
+  element.appendChild(myjpg);
+
+  console.log(Data);
 
   return element;
 }
