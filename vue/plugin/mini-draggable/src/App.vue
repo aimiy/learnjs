8f5e6961c8c2647e32442127306641b98fa59c6f
f <fuym@yuntongxun.com>
2019-07-06 18:25:36 +0800

    draggable

--- vue/plugin/mini-draggable/src/App.vue
+++ vue/plugin/mini-draggable/src/App.vue
@@ -1,25 +1,33 @@
 <template>
   <div id="app">
-    <draggable />
+    <draggable ref="dra" title="test" @con="con1" />
   </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import draggable from './lib/index.js';
-Vue.use(draggable)
+import draggable from "./lib/index.js";
+import Vue from "vue";
+Vue.use(draggable);
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    draggable
+  },
+  methods: {
+    con1(val) {
+      console.log(val);
+      
+      console.log(987);
+      this.$refs["dra"].$con();
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ vue/plugin/mini-draggable/src/lib/mini-draggable.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="draggable">
+    <div class="title" @click="log(title)">{{title}}</div>
+    <div class="content"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "miniDraggable",
+  props: ["title"],
+  methods:{
+      log(val){
+          this.$emit('con',val);
+      },
+      $con(){
+          console.log(564);
+      }
+  }
+};
+</script>
+<style lang="less" scoped>
+.draggable{
+    .title{
+        color: #cecece;
+    }
+}
+
+</style>
+
+
