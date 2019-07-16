94b9cf6757177a7b315a5aa0bab55252e2376a02
f <fuym@yuntongxun.com>
2019-07-16 17:26:50 +0800

    npm test

--- vue/plugin/mini-draggable/src/lib/index.js
+++ vue/plugin/mini-draggable/src/lib/index.js
@@ -1,6 +1,6 @@
 import miniDraggable from './mini-draggable.vue';
 
-miniDraggable.install = function (Vue, options) {
+miniDraggable.install = function (Vue) {
     // 1. 添加全局方法或属性
     Vue.myGlobalMethod = function () {
       // 逻辑...
@@ -8,7 +8,7 @@ miniDraggable.install = function (Vue, options) {
   
     // 2. 添加全局资源
     Vue.directive('my-directive', {
-      bind (el, binding, vnode, oldVnode) {
+      bind () {
         // 逻辑...
       }
       
@@ -18,14 +18,14 @@ miniDraggable.install = function (Vue, options) {
     Vue.mixin({
       created: function () {
         // 逻辑..
-        console.log(123);
+        // console.log(123);
         
       }
       
     })
   
     // 4. 添加实例方法
-    Vue.prototype.$myMethod = function (methodOptions) {
+    Vue.prototype.$myMethod = function () {
       // 逻辑...
     }
   }
