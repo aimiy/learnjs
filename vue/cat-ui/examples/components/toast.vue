873a6f12e05bf8fef70bfdb9107b281f2eba3ba7
aimiy <mioly@foxmail.com>
2019-08-28 18:37:23 +0800

    catui

---
+++ vue/cat-ui/examples/components/toast.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <cat-button type="primary" @click="showTop">提示top位</cat-button>
+    <cat-button type="primary" @click="showMiddle">点击middle位</cat-button>
+    <cat-button type="primary" @click="showBottom">提示bottom位</cat-button>
+    <cat-button type="primary" @click="showMiddle">点击提示带icon</cat-button>
+  </div>
+</template>
+
+<script>
+import { Toast } from "../../packages/index.js";
+
+export default {
+  name: "HelloWorld",
+  methods: {
+    showTop(){
+      this.$toast({
+        message: "全局引用",
+        position: "top",
+        duration: 5000
+      });
+    },
+    showMiddle() {
+      this.$toast({
+        message: "全局引用",
+        position: "middle",
+        duration: 5000
+      });
+    },
+    showBottom(){
+      this.$toast({
+        message: "全局引用",
+        position: "bottom",
+        duration: 5000
+      });
+    }
+  },
+  created() {
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
