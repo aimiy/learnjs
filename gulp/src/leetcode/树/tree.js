8ecb253b9e05091f25688037aac39d00c4501e53
aimiy <mioly@foxmail.com>
2020-03-10 01:26:32 +0800

    最长同值路径

---
+++ gulp/src/leetcode/树/tree.js
@@ -0,0 +1,11 @@
+let changeTree = {
+    arrToObj:function(arr){
+        return {}
+    },
+    arrToPic:function(arr){
+        return console.log("  4\n / \\\n5   6")
+    },
+    objToPic:function(obj){
+        return console.log("  4\n / \\\n5   6")
+    }
+}
